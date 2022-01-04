9349291dcaaef6b20fe85349f05112ef7212309e
Bohdan Kryven <bodyan1108@gmail.com>
2022-01-04 22:22:33 +0200

    added done lecture online task

--- flask_app/app.py
+++ flask_app/app.py
@@ -1,15 +1,38 @@
-from flask import Flask
-import json
-import requests
+from flask import Flask, render_template, request, redirect
 
 app = Flask(__name__)
 
+text_global = []
 
-@app.route('/')
+
+@app.route('/', methods=["GET", "POST"])
 def index():
-    response = requests.get('http://app1:8080/')
-    print(response.content)
-    return response.json()[1]['first_name']
+    return render_template('index.html')
 
 
-app.run(host='0.0.0.0', port=8081, debug=True)
+@app.route('/display-text/<txt>')
+def text(txt):
+    txt = txt.split()
+    return render_template('text.html', text111=txt[0])
+
+
+@app.route('/display-saved-text', methods=['GET'])
+def save_text():
+    global text_global
+    return render_template('saved_text.html', text=text_global)
+
+
+@app.route('/text/new', methods=['GET'])
+def text_new():
+    return render_template('text_form.html')
+
+
+@app.route('/save', methods=["POST"])
+def save():
+    global text_global
+    form = request.form
+    text_global = form['text_blablabla']
+    return redirect('/display-saved-text')
+
+
+app.run(host='0.0.0.0', port=8080, debug=True)
