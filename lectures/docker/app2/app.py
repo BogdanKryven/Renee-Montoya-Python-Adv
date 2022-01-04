6c5dbd28e674ab4d8b04b5da1e4c2d7c1dc8622f
Bohdan Kryven <bodyan1108@gmail.com>
2022-01-04 22:23:57 +0200

    updated info

--- lectures/docker/app2/app.py
+++ lectures/docker/app2/app.py
@@ -4,8 +4,10 @@ import requests
 
 app = Flask(__name__)
 
+
 @app.route('/hello', methods=["GET", "POST"])
 def index():
     return "Hello World!"
+
 
 app.run(host='0.0.0.0', port=8081, debug=True)
